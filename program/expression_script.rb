e3464169a84d57a666cb7a6502ee32c1bc1b12b0
fuurin <komatsu0624@docomo.ne/jp>
2015-05-20 14:22:46 +0900

    pull

--- program/expression_script.rb
+++ program/expression_script.rb
@@ -225,10 +225,13 @@ class Add
 	def reduce
 		if left.reducible?
 			Add.new(left.reduce, right)
+
 		elsif right.reducible?
 			Add.new(left, right.reduce)
+		
 		else
 			Number.new(left.value + right.value)
+		
 		end
 	end
 end
