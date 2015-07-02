985e9631615bc3dfe0ec0d9dc318f3f601a00913
polyome <s39339@sendai-nct.jp>
2015-07-02 12:02:22 +0900

    add NFA in The Simplest Computer

--- program/The_Simplest_Computer.rb
+++ program/The_Simplest_Computer.rb
@@ -136,3 +136,130 @@ end
 #=> false
 #>> dfa_design.accepts?('baba')
 #=> true
+
+
+##Nondeterministic Finite Automata
+
+##非決定性有限オートマトン##
+
+require 'set'
+class NFARulebook < Struct.new(:rules)
+	def next_states(states, character)
+		states.flat_map { |state| follow_rules_for(stat
+	end
+	def follow_rules_for(state, character)
+		rules_for(state, character).map(&:follow)
+	end
+	def rules_for(state, character)
+		rules.select { |rule| rule.applies_to?(state, c
+	end
+end
+#>> rulebook = NFARulebook.new([
+#FARule.new(1, 'a', 1), FARule.new(1, 'b', 1), FARule.n
+#FARule.new(2, 'a', 3), FARule.new(2, 'b', 3),
+#FARule.new(3, 'a', 4), FARule.new(3, 'b', 4)
+#])
+#=> #<struct NFARulebook rules=[…]>
+#>> rulebook.next_states(Set[1], 'b')
+#=> #<Set: {1, 2}>
+#>> rulebook.next_states(Set[1, 2], 'a')
+#=> #<Set: {1, 3}>
+#>> rulebook.next_states(Set[1, 3], 'b')
+#=> #<Set: {1, 2, 4}>
+
+class NFA < Struct.new(:current_states, :accept_states,
+	def accepting?
+		(current_states & accept_states).any?
+	end
+end
+
+#>> NFA.new(Set[1], [4], rulebook).accepting?
+#=> false
+#>> NFA.new(Set[1, 2, 4], [4], rulebook).accepting?
+#=> true
+
+class NFA
+	def read_character(character)
+		self.current_states = rulebook.next_states(curr
+	end
+	def read_string(string)
+		string.chars.each do |character|
+		read_character(character)
+		end
+	end
+end
+
+#>> nfa = NFA.new(Set[1], [4], rulebook); nfa.accepting
+#=> false
+#>> nfa.read_character('b'); nfa.accepting?
+#=> false
+#>> nfa.read_character('a'); nfa.accepting?
+#=> false
+#>> nfa.read_character('b'); nfa.accepting?
+#=> true
+#>> nfa = NFA.new(Set[1], [4], rulebook)
+#=> #<struct NFA current_states=#<Set: {1}>, accept_sta
+#>> nfa.accepting?
+#=> false
+#>> nfa.read_string('bbbbb'); nfa.accepting?
+#=> true
+
+class NFADesign < Struct.new(:start_state, :accept_stat
+	def accepts?(string)
+		to_nfa.tap { |nfa| nfa.read_string(string) }.ac
+	end
+	def to_nfa
+		NFA.new(Set[start_state], accept_states, rulebo
+	end
+end
+
+#>> nfa_design = NFADesign.new(1, [4], rulebook)
+#=> #<struct NFADesign start_state=1, accept_states=[4]
+#>> nfa_design.accepts?('bab')
+#=> true
+#>> nfa_design.accepts?('bbbbb')
+#=> true
+#>> nfa_design.accepts?('bbabb')
+#=> false
+#>> rulebook = NFARulebook.new([
+#FARule.new(1, nil, 2), FARule.new(1, nil, 4),
+#FARule.new(2, 'a', 3),
+#FARule.new(3, 'a', 2),
+#FARule.new(4, 'a', 5),
+#FARule.new(5, 'a', 6),
+#FARule.new(6, 'a', 4)
+#])
+#=> #<struct NFARulebook rules=[…]>
+#>> rulebook.next_states(Set[1], nil)
+#=> #<Set: {2, 4}>
+
+class NFARulebook
+	def follow_free_moves(states)
+		more_states = next_states(states, nil)
+		if more_states.subset?(states)
+			states
+		else
+			follow_free_moves(states + more_states)
+		end
+	end
+end
+
+#>> rulebook.follow_free_moves(Set[1])
+#=> #<Set: {1, 2, 4}>
+
+class NFA
+	def current_states
+		rulebook.follow_free_moves(super)
+	end
+end
+
+#>> nfa_design = NFADesign.new(1, [2, 4], rulebook)
+#=> #<struct NFADesign …>
+#>> nfa_design.accepts?('aa')
+#=> true
+#>> nfa_design.accepts?('aaa')
+#=> true
+#>> nfa_design.accepts?('aaaaa')
+#=> false
+#>> nfa_design.accepts?('aaaaaa')
+#=> true
