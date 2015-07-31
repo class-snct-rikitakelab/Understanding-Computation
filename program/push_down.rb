e3098f8619e88c7751151582ef07776ce50887e3
fuurin <s1100617@sendai-nct.jp>
2015-07-31 15:43:21 +0900

    test3

--- program/push_down.rb
+++ program/push_down.rb
@@ -189,3 +189,122 @@ configuration = PDAConfiguration.new(2, Stack.new(['$']))
 
 rulebook.follow_free_moves(configuration)
 # #<struct PDAConfiguration state=1, stack=#<Stack ($)>>
+
+DPDARulebook.new([PDARule.new(1, nil, 1, '$', ['$'])]).
+	follow_free_moves(PDAConfiguration.new(1, Stack.new(['$'])))
+# SystemStackError: stack level too deep
+
+
+
+class DPDA
+	def current_configuration
+		rulebook.follow_free_moves(super)
+	end
+end
+
+
+# TEST 7
+dpda = DPDA.new(PDAConfiguration.new(1, Stack.new(['$'])), [1], rulebook)
+# #<struct DPDA ...>
+
+dpda.read_string('(()('); dpda.accepting?
+# false
+
+dpda.current_configuration
+# #<struct PDAConfiguration state=2, stack=#<Stack (b)b$>>
+
+dpda.read_string('))()'); dpda.accepting?
+# true
+
+dpda.current_configuration
+# #<struct PDAConfiguration state=1, stack=#<Stack ($)>>
+
+
+
+class DPDADesign < Struct.new(:start_state, :bottom_character,
+							:accept_states, :rulebook)
+	def accepts?(string)
+		to_dpda.tap { |dpda| dpda.read_string(string) }.accepting?
+	end
+
+	def to_dpda
+		start_stack = Stack.new([bottom_character])
+		start_configuration = PDAConfiguration.new(start_state, start_stack)
+		DPDA.new(start_configuration, accept_states, rulebook)
+	end
+end
+
+
+
+# TEST 8
+
+dpda_design = DPDADesign.new(1, '$', [1], rulebook)
+# #<struct DPDADesign ...>
+
+dpda_design.accepts?('(((((((((())))))))))')
+# true
+
+dpda_design.accepts?('()(())((()))(()(()))')
+# true
+
+dpda_design.accepts?('(()(()(()()(()()))()')
+# false
+
+dpda_design.accepts?('(()')
+# NoMethodError: ...
+
+
+
+class PDAConfiguration
+	STUCK_STATE = Object.new
+
+	def stuck
+		PDAConfiguration.new(STUCK_STATE, stack)
+	end
+
+	def stuck?
+		state == STUCK_STATE
+	end
+end
+
+class DPDA
+	def next_configuration(character)
+		if rulebook.applies_to?(current_configuration, bottom_character)
+			rulebook.next_configuration(current_configuration, character)
+		else
+			current_configuration.stuck
+		end
+	end
+
+	def stuck?
+		current_configuration.stuck?
+	end
+
+	def read_character(character)
+		self.current_configuration = next_configuration(character)
+	end
+
+	def read_string(string)
+		string.chars.each do |character|
+			read_character(character) unless stuck?
+		end
+	end
+end
+
+
+
+#TEST 9
+dpda = DPDA.new(PDAConfiguration.new(1, Stack.new(['$'])), [1], rulebook)
+# #<struct DPDA ...>
+
+dpda.read_string('())'); dpda.current_configuration
+# #<struct PDAConfiguration state=#<Object>, stack=#<Stack ($)>>
+
+dpda.accepting?
+# false
+
+dpda.stuck?
+# true
+
+dpda_design.accepts?('())')
+# false
